2268eeb9a6a46f902a48697f875a26d66870f1c3
xujj277 <939160741@qq.com>
2019-01-25 11:29:18 +0800

    controller 来控制 view

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,18 +1,21 @@
 !function () {
   var view = document.querySelector('#mySlides')
-  var mySwiper = new Swiper (view.querySelector('.swiper-container'), {
-    // Optional parameters
-    loop: true,
+  var controller = function (view) {
+    var mySwiper = new Swiper (view.querySelector('.swiper-container'), {
+      // Optional parameters
+      loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  })
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    })
+  }
+  controller(view)
 }.call()
--- js/sticky-torbar.js
+++ js/sticky-torbar.js
@@ -1,12 +1,18 @@
 !function () {
   var view = document.querySelector('#topNavBar')
-
-  window.addEventListener('scroll', function () {
-    if (window.scrollY > 0) {
-      view.classList.add('sticky')
-    } else {
-      view.classList.remove('sticky')
+  var controller = {
+    view: null,
+    init: function (view) {
+      var view = this.view
+      window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+          view.classList.add('sticky')
+        } else {
+          view.classList.remove('sticky')
+        }
+      })
     }
-  })
+  }
+  controller.init(view)
 }.call()
 
