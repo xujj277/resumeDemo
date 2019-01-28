cf2f45ad0934f21a1bbae7b71b00cb9b0d7480eb
xujj277 <939160741@qq.com>
2019-01-28 10:35:57 +0800

    mvc整理

--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,35 +1,48 @@
 !function () {
   var view = document.querySelector('nav.menu')
-  var controller = function (view) {
-    let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-    function animate(time) {
+  var controller = {
+    view: null,
+    aTags: null,
+    init: function (view) {
+      this.view = view
+      this.initAnimation()
+      this.bindEvents()
+    },
+    initAnimation: function () {
+      function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+      }
       requestAnimationFrame(animate);
-      TWEEN.update(time);
-    }
-    requestAnimationFrame(animate);
+    },
+    scrollToElement: function () {
+      let top = document.querySelector(x.currentTarget.getAttribute('href')).offsetTop
+      let currentTop = window.scrollY
+      let targetTop = top - 80
+      let s = targetTop - currentTop
+      if (s > 500) {
+        s = 500
+      }
+      let t = Math.abs((s/100)*200)
 
-    for (let i=0; i<aTags.length; i++) {
-      aTags[i].onclick = function (x) {
-        x.preventDefault();
-        let top = document.querySelector(x.currentTarget.getAttribute('href')).offsetTop
-        let currentTop = window.scrollY
-        let targetTop = top - 80
-        let s = targetTop - currentTop
-        if (s > 500) {
-          s = 500
+      var coords = { y: currentTop };
+      var tween = new TWEEN.Tween(coords)
+        .to({ y: targetTop }, t)
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(function() {
+          window.scrollTo(0, coords.y)
+        })
+        .start();
+    },
+    bindEvents: function () {
+      this.aTags = document.querySelectorAll('nav.menu > ul > li > a')
+      for (let i=0; i<this.aTags.length; i++) {
+        this.aTags[i].onclick = function (x) {
+          x.preventDefault();
+          this.scrollToElement()
         }
-        let t = Math.abs((s/100)*200)
-
-        var coords = { y: currentTop };
-        var tween = new TWEEN.Tween(coords)
-          .to({ y: targetTop }, t)
-          .easing(TWEEN.Easing.Quadratic.InOut)
-          .onUpdate(function() {
-            window.scrollTo(0, coords.y)
-          })
-          .start();
       }
     }
   }
-  controller(view)
+  controller.init(view)
 }.call()
--- js/sticky-torbar.js
+++ js/sticky-torbar.js
@@ -1,18 +1,30 @@
 !function () {
-  var view = document.querySelector('#topNavBar')
+  var view = document.querySelector('.topNavBar')
   var controller = {
     view: null,
     init: function (view) {
+      this.view = view
+      this.bindEvents()
+      // this.bindEvents.call(this)
+    },
+    bindEvents: function () {
       var view = this.view
-      window.addEventListener('scroll', function () {
+      window.addEventListener('scroll', (x) => {
         if (window.scrollY > 0) {
-          view.classList.add('sticky')
+          this.active()
         } else {
-          view.classList.remove('sticky')
+          this.deactive()
         }
       })
+    },
+    active: function () {
+      this.view.classList.add('sticky')
+    },
+    deactive: function () {
+      this.view.classList.remove('sticky')
     }
   }
   controller.init(view)
+  // controller.init.call(controller, view)
 }.call()
 
